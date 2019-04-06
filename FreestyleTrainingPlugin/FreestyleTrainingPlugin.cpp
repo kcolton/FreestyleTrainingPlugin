d0b433c1af6a9b5d250593abef750a24d465fe0d
Ken Colton <kcolton@gmail.com>
2019-04-06 19:40:46 -0400

    Add direction control so can avoid slow rolls

--- FreestyleTrainingPlugin/FreestyleTrainingPlugin.cpp
+++ FreestyleTrainingPlugin/FreestyleTrainingPlugin.cpp
@@ -3,8 +3,14 @@
 #include "bakkesmod\wrappers\GameEvent\TutorialWrapper.h"
 #include "bakkesmod\wrappers\GameObject\CarWrapper.h"
 #include "bakkesmod\wrappers\GameObject\BallWrapper.h"
+#include <sstream>
+#include <iomanip>
 
 #define SPINYOUBITCHDEADZONE 0.2f
+
+#define LEFT_AND_RIGHT 0
+#define LEFT 1
+#define RIGHT 2
 
 BAKKESMOD_PLUGIN(FreestyleTrainingPlugin, "Freestyle Training Plugin", "0.1", PLUGINTYPE_FREEPLAY)
 
@@ -17,12 +23,15 @@ void FreestyleTrainingPlugin::onLoad()
 {
 	cvarManager->log("FreestyleTrainingPlugin::onLoad");
 
+	srand(time(NULL));
+
 	enabled = make_shared<bool>(false);
 	autoAirRoll = make_shared<float>(0.f);
 
 	randomizeAutoAirRollEnabled = make_shared<bool>(false);
 	randomizeAutoAirRollLower = make_shared<float>(0.0f);
 	randomizeAutoAirRollUpper = make_shared<float>(0.0f);
+	randomizeAutoAirRollDirection = make_shared<int>(0);
 
 	// freeplay is now a mutator
 	gameWrapper->HookEvent("Function TAGame.Mutator_Freeplay_TA.Init", bind(&FreestyleTrainingPlugin::OnFreeplayLoad, this, std::placeholders::_1));
@@ -34,9 +43,35 @@ void FreestyleTrainingPlugin::onLoad()
 
 	cvarManager->registerCvar("freestyletraining_auto_air_roll", "0", "Auto air roll amount", true, true, -1.f, true, 1.f).bindTo(autoAirRoll);
 
+	// todo: combine enabled and direction 0 = off, 1 = left, 2 = right, 3 = left and right. then can use bool ops and look cool
 	cvarManager->registerCvar("freestyletraining_randomize_auto_air_roll_enabled", "0", "Enables/disable auto air roll randomization", true, true, 0.f, true, 1.f).bindTo(randomizeAutoAirRollEnabled);
-	cvarManager->registerCvar("freestyletraining_randomize_auto_air_roll_lower", "0", "Random Auto Air Roll Lower", true, true, -1.f, true, 1.f).bindTo(randomizeAutoAirRollLower);
-	cvarManager->registerCvar("freestyletraining_randomize_auto_air_roll_upper", "0", "Random Auto Air Roll Upper", true, true, -1.f, true, 1.f).bindTo(randomizeAutoAirRollUpper);
+	cvarManager->getCvar("freestyletraining_randomize_auto_air_roll_enabled").addOnValueChanged([this](string oldValue, CVarWrapper cvar)
+	{
+		if (cvar.getBoolValue()) {
+			string direction;
+			switch (*randomizeAutoAirRollDirection) {
+			case LEFT_AND_RIGHT:
+				direction = "Left & Right";
+				break;
+			case LEFT:
+				direction = "Left Only";
+				break;
+			case RIGHT:
+				direction = "Right Only";
+				break;
+			}
+
+			Log("Random Auto Air Roll ON - " + direction, true);
+			DoRandomizeAutoAirRoll();
+		}
+		else {
+			Log("Random Auto Air Roll OFF", true);
+		}
+	});
+
+	cvarManager->registerCvar("freestyletraining_randomize_auto_air_roll_lower", "0", "Random Auto Air Roll Lower", true, true, 0.0f, true, 1.f).bindTo(randomizeAutoAirRollLower);
+	cvarManager->registerCvar("freestyletraining_randomize_auto_air_roll_upper", "0", "Random Auto Air Roll Upper", true, true, 0.0f, true, 1.f).bindTo(randomizeAutoAirRollUpper);
+	cvarManager->registerCvar("freestyletraining_randomize_auto_air_roll_direction", "0", "Random Auto Air Roll Direction", false, false, 0, false, 0, false).bindTo(randomizeAutoAirRollDirection);
 
 	cvarManager->registerNotifier("freestyletraining_do_randomize_auto_air_roll", std::bind(&FreestyleTrainingPlugin::DoRandomizeAutoAirRoll, this), "Randomize auto air roll within upper and lower", PERMISSION_FREEPLAY);
 }
@@ -47,11 +82,8 @@ void FreestyleTrainingPlugin::onUnload()
 	cvarManager->log("FreestyleTrainingPlugin::onUnload");
 }
 
-// todo: rename since is not hooked OnPreAsync, was a copy paste mistake
-void FreestyleTrainingPlugin::OnPreAsync(CarWrapper cw, void * params, string funcName) 
-{
-	//cvarManager->log("FreestyleTrainingPlugin::OnPreAsync");
-	
+void FreestyleTrainingPlugin::SetVehicleInput(CarWrapper cw, void * params, string funcName)
+{	
 	if (!gameWrapper->IsInFreeplay())
 	{
 		cvarManager->log("not in freeplay but freestyle training still hooked somehow");
@@ -64,14 +96,11 @@ void FreestyleTrainingPlugin::OnPreAsync(CarWrapper cw, void * params, string fu
 	if (abs(ci->Roll) < SPINYOUBITCHDEADZONE) {
 		ci->Roll = std::max(-1.0f, std::min(*autoAirRoll, 1.0f));
 		RandomizeAirRollTick(true);
-		// cvarManager->log("air roll overridden to:" + to_string(ci->Roll));
 	}
 	else 
 	{
 		RandomizeAirRollTick(false);
 	}
-	
-	//cvarManager->log("auto set roll to:" + to_string(ci->Roll));
 }
 
 
@@ -80,6 +109,7 @@ void FreestyleTrainingPlugin::OnFreeplayLoad(std::string eventName)
 	cvarManager->log("FreestyleTrainingPlugin::OnFreeplayLoad eventName:" + eventName);
 	if (*enabled)
 	{
+		gameWrapper->LogToChatbox("Freestyle Training Plugin ON");
 		Hook();
 	}
 }
@@ -95,10 +125,12 @@ void FreestyleTrainingPlugin::OnEnabledChanged(std::string oldValue, CVarWrapper
 	cvarManager->log("FreestyleTrainingPlugin::OnEnabledChanged oldValue:" + oldValue + " current:" + to_string(cvar.getBoolValue()));
 	if (cvar.getBoolValue() && gameWrapper->IsInFreeplay())
 	{
+		gameWrapper->LogToChatbox("Freestyle Training Plugin Enabled");
 		Hook();
 	}
 	else
 	{
+		gameWrapper->LogToChatbox("Freestyle Training Plugin OFF");
 		Unhook();
 	}
 }
@@ -108,7 +140,7 @@ void FreestyleTrainingPlugin::Hook()
 	cvarManager->log("FreestyleTrainingPlugin::Hook");
 	// copied from mechanical plugin
 	gameWrapper->HookEventWithCaller<CarWrapper>("Function TAGame.Car_TA.SetVehicleInput",
-		bind(&FreestyleTrainingPlugin::OnPreAsync, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+		bind(&FreestyleTrainingPlugin::SetVehicleInput, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
 }
 
 void FreestyleTrainingPlugin::Unhook() 
@@ -120,9 +152,6 @@ void FreestyleTrainingPlugin::Unhook()
 void FreestyleTrainingPlugin::RandomizeAirRollTick(bool usedAutoAirRollThisTick)
 {
 	// randomize auto air roll amount each time user finishes overriding
-	
-	// cvarManager->log("RandomizeAirRollTick usedAutoAirRollLastTick:" + to_string(usedAutoAirRollLastTick) + " usedAutoAirRollThisTick:" + to_string(usedAutoAirRollThisTick) + " randomizeAutoAirRollEnabled:" + to_string(*randomizeAutoAirRollEnabled));
-
 	if (usedAutoAirRollLastTick && !usedAutoAirRollThisTick && *randomizeAutoAirRollEnabled)
 	{
 		DoRandomizeAutoAirRoll();
@@ -136,9 +165,42 @@ void FreestyleTrainingPlugin::DoRandomizeAutoAirRoll()
 	float randomMin = min(*randomizeAutoAirRollLower, *randomizeAutoAirRollUpper);
 	float randomMax = max(*randomizeAutoAirRollLower, *randomizeAutoAirRollUpper);
 
-	float randomized = random(randomMin, randomMax);
+	float randomized = abs(random(randomMin, randomMax));
 
-	//cvarManager->log("DoRandomizeAutoAirRoll randomMin: " + to_string(randomMin) + " randomMax:" + to_string(randomMax) + " randomized:" + to_string(randomized));
+	switch (*randomizeAutoAirRollDirection) {
+	case LEFT_AND_RIGHT:
+		if (rand() % 2 == 0) {
+			// 50% chance of randomizing left
+			randomized = -1.0f * randomized;
+		}
+	case LEFT:
+		randomized = -1.0f * randomized;
+		break;
+	case RIGHT:
+		break;
+	}
+	
+	// todo: maybe delay this chat until auto air roll takes affect again (may be still overriding)
+	stringstream chatMsg;
+	chatMsg << "Random Auto Air Roll:";
+	chatMsg << setprecision(2) << randomized;
+
+	if (randomized < 0) {
+		chatMsg << " (left)";
+	}
+	else if (randomized > 0) {
+		chatMsg << " (right)";
+	}
+
+	Log(chatMsg.str(), true);
 
 	cvarManager->getCvar("freestyletraining_auto_air_roll").setValue(randomized);
+}
+
+void FreestyleTrainingPlugin::Log(string message, bool sendToChat) 
+{
+	cvarManager->log(message);
+	if (sendToChat) {
+		gameWrapper->LogToChatbox(message, "BM");
+	}
 }
--- FreestyleTrainingPlugin/FreestyleTrainingPlugin.h
+++ FreestyleTrainingPlugin/FreestyleTrainingPlugin.h
@@ -11,6 +11,7 @@ private:
 	shared_ptr<bool> randomizeAutoAirRollEnabled;
 	shared_ptr<float> randomizeAutoAirRollLower;
 	shared_ptr<float> randomizeAutoAirRollUpper;
+	shared_ptr<int> randomizeAutoAirRollDirection;
 
 	bool usedAutoAirRollLastTick;
 
@@ -25,9 +26,10 @@ public:
 	virtual void onLoad();
 	virtual void onUnload();
 	
-	void OnPreAsync(CarWrapper cw, void * params, string funcName);
+	void SetVehicleInput(CarWrapper cw, void * params, string funcName);
 	void OnFreeplayLoad(std::string eventName);
 	void OnFreeplayDestroy(std::string eventName);
 	void OnEnabledChanged(std::string oldValue, CVarWrapper cvar);
+	void Log(std::string message, bool sendToChat);
 };
 
